36e46f7eab7b8ed93b6fcc2ca7ec8d93328d658e
Dishant Garg <dishantgarg@Dishants-Laptop.local>
2024-07-28 18:13:59 +0530

    done folder structure

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+const moongoose = require("mongoose");
+
+
+moongoose.connect("mongodb+srv://dgarg9803:dl4EYObHndpIrA77@cluster0.rlj0nnd.mongodb.net/")
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+}
+
+);
